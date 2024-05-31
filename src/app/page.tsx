f607a0180e3c5c94c9eac90255dd9f3e674abb89
camziny <cameronziny@gmail.com>
2024-05-31 15:40:57 -0400

    - basic ui - set bg - edit read me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const imageUrls = [
+  "https://utfs.io/f/90ba1135-a67e-4dd6-9615-71bb5634ec07-hzt98r.jpeg",
+];
+
+const images = imageUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      muscare
+    <main>
+      <h1 className="text-center text-3xl font-bold text-stone-700">
+        inchallah your kids will be ok
+      </h1>
+      <div className="flex flex-wrap justify-center gap-4">
+        {images.map((image) => (
+          <div key={image.id} className="w-1/3 p-4">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
